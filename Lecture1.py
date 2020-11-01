0783bc1ba1aa1807f3013c8920613de3548834c8
Atabek <atabekarapov14@gmail.com>
2020-11-01 17:40:01 +0600

    Week2_Lecture1

---
+++ Lecture1.py
@@ -0,0 +1,61 @@
+# # name = input ( " Enter your name: ").title()
+# # # name = name.lower()
+# # # name = name.upper()
+# # # name = name.capitalize()
+# # # name = name.title() (change the first words alphabit)
+# # # print (type (name))
+# # print (name)
+
+
+
+# # some_string = "atabek world"
+# # .find('u'))
+# # .rfind('m')) (from left to the right)
+# # .rindex('p'))
+#  (some_string.count('a'))  (count the number of alphabit from the word)
+# # print(dir("")) (open all the indexs in python or codes in python)
+# # replace('a', 'y')   (change the alphabit to the other one)
+# # split('-') (deleted what we write on the brackets, razdelit)
+# # upper().split('-')   ()
+#join --> ('a', 'b', 'c') --> obedinayet
+# some_str = input( " Enter some sting: ")
+# print(some_str)
+# print(type(some_str))
+# print(len(some_str))
+# print(len(some_str.split()))
+# .isalpha)   ( only for alphabits that is a TRUE or FALSE)
+# .isdigit)  (only for numbers that is a TRUE or FALSE)
+# .isspace())   (состоит ли строка только пробелов)
+# .strip())
+
+# name = input(" Enter your name: ")
+# age = input("Enter your age: ")
+        #    Formatirovanie
+
+# %3
+# %d
+
+# print("Hello, " + name. Your age is',   age)
+
+# print(f"Hello, {name}. Your age is {age}.")
+# print('Hello, {1}. Your age is {0}.' .format(age, name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
